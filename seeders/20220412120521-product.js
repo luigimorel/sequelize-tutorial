49f9f80e4c8f7e559ff899e6d9ea068b2e7282b4
Luigi Morel <morelmiles@gmail.com>
2022-04-12 22:33:42 +0300

    add associations

--- seeders/20220412120521-product.js
+++ seeders/20220412120521-product.js
@@ -10,6 +10,7 @@ module.exports = {
           name: 'Bread',
           quantity: 4,
           inStock: true,
+          categoryId: 'a52467a3-3a71-45c4-bf1c-9ace5ad3668f',
           productImage:
             'https://res.cloudinary.com/morelmiles/image/upload/v1649765314/download_nwfpru.jpg',
           // Store the price in cents e.g if price is $5, multiply by 100 cents i.e 5 * 100 = 500 cents
@@ -22,6 +23,7 @@ module.exports = {
           id: '9df55a7c-772c-459f-a21b-933a96981ca6',
           name: 'Milk',
           quantity: 4,
+          categoryId: '33a9e6e0-9395-4f6c-b1cd-3cf1f87e195a',
           inStock: true,
           productImage:
             'https://res.cloudinary.com/morelmiles/image/upload/v1647356184/milk_ckku96.jpg',
