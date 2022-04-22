90eb4ea3a981f68ed4c39ad61eb9bc3b53eed2ef
Luigi Morel <morelmiles@gmail.com>
2022-04-22 10:02:24 +0300

    fix: format and add comments where needed

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// Disable dotenv in production
+// Disable dotenv in a production env
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -10,23 +10,29 @@ const helmet = require('helmet')
 
 // Routes import
 const routes = require('./routes')
+
+// Initialize express
 const app = express()
 
+// Port if PORT env variable does not exist in .env
 const port = 3000
 
+// CORS
 const corsOptions = {
   origin: `http://localhost:${process.env.PORT || port}`
 }
 
 // Middleware
-
 app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(logger('combined'))
+app.use(logger('dev'))
 app.use(helmet())
+
+// Routes middleware
 app.use('/api', routes)
 
+// Port listener
 app.listen(process.env.PORT || port, () => {
   console.log(`Server is running on ${process.env.PORT || port}`)
 })
